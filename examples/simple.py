94b9aae0f3179e365b02822359f561d5f2e2b678
Corey McCandless <crm1994@gmail.com>
2018-02-20 15:59:34 -0600

    add examples

---
+++ examples/simple.py
@@ -0,0 +1,24 @@
+"""
+$ python simple.py foo_value -b bar_value -h
+usage: simple [-h] [-b BAR] foo
+
+positional arguments:
+  foo                positional argument
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -b BAR, --bar BAR  optional argument
+                     with multiline help (default: default_for_bar)
+
+examples:
+    foo_value                                   positional arg only
+    foo_value -b bar_value                      with optional arg
+    foo_value --bar bar_value                   optional arg long-form
+"""
+from __future__ import print_function
+import argutil
+
+argparser = argutil.get_parser(__file__)
+opts = argparser.parse_args()
+print('foo:', opts.foo)
+print('bar:', opts.bar)
---
+++ examples/submodules.py
@@ -0,0 +1,68 @@
+"""
+$ python submodules.py -h
+usage: submodules [-h] [-c CONFIG] {this,that} ...
+
+positional arguments:
+  {this,that}
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c CONFIG, --config CONFIG
+                        global option (default: None)
+
+examples:
+    this                                        subcommand
+    -c config.cfg that                          subcommand with global option
+
+
+$ python submodules.py this -h
+usage: this [-h] [--then THEN] [--foo]
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --then THEN  option for this (default: None)
+  --foo        this-exclusive flag (default: False)
+
+examples:
+    --then that                                 this then that
+
+
+$ python submodules.py that -h
+usage: that [-h] [--then THEN]
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --then THEN  option for that (default: None)
+
+examples:
+    --then this                                 that then this
+"""
+from __future__ import print_function
+import argutil
+
+
+def this(opts):
+    if opts.config:
+        print('config:', opts.config)
+    print('this was called')
+    if opts.then:
+        print('then:', opts.then)
+    if opts.foo:
+        print('foo set')
+
+
+def that(opts):
+    if opts.config:
+        print('config:', opts.config)
+    print('that was called')
+    if opts.then:
+        print('then:', opts.then)
+
+
+env = {
+    'this': this,
+    'that': that
+}
+parser = argutil.get_parser(__file__, env)
+opts = parser.parse_args()
+opts.func(opts)
