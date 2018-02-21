dd931223868f13b89dc2a3f71410163cb14ade83
Corey McCandless <crm1994@gmail.com>
2018-02-21 09:23:22 -0600

    bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='argutil',
-    version='1.0.0',
+    version='1.0.1',
     description='Wrapper for argparse that uses JSON config files to define command line parsers',
     author='cmccandless',
     license='MIT',
