778b5206d9770a7912ebcec7d8102f9654388728
Corey McCandless <crm1994@gmail.com>
2018-02-23 10:53:39 -0600

    handle long_description in setup.py instead of setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 from setuptools.config import read_configuration
+import pypandoc
 
 conf_dict = read_configuration('setup.cfg')
-long_desc = conf_dict['long_description']
-try:
-    import pypandoc
-    long_desc = pypandoc.convert_text(long_desc, 'rst', 'md')
-except (IOError, ImportError):
-    pass
+long_desc = '\n'.join(
+    pypandoc.convert('README.md', 'rst'),
+    pypandoc.convert('LICENSE', 'rst', 'md')
+)
 
 setup(long_description=long_desc)
