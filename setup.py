7def3a10ab7016e42262ccad31a4945c0e739946
Corey McCandless <crm1994@gmail.com>
2018-02-23 10:40:02 -0600

    convert long description to RST

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
+from setuptools.config import read_configuration
 
-setup()
+conf_dict = read_configuration('setup.cfg')
+long_desc = conf_dict['long_description']
+try:
+    import pypandoc
+    long_desc = pypandoc.convert_text(long_desc, 'rst', 'md')
+except (IOError, ImportError):
+    pass
+
+setup(long_description=long_desc)
