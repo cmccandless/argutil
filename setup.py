fb280eae8aa341eba3fe1ca080481cb7944163fc
Corey McCandless <crm1994@gmail.com>
2018-02-23 09:58:51 -0600

    configure setup.cfg

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='argutil',
-    version='1.0.1',
-    description='Wrapper for argparse that uses JSON config files to define command line parsers',
-    author='cmccandless',
-    license='MIT',
-    url='http://github.com/cmccandless/argutil',
-    packages=['argutil', 'tests'],
-)
+setup()
